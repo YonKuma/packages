61425ed0cc736f65b2522f48e281cb7238449142
Fewtarius <fewtarius@gmail.com>
2020-12-31 07:33:49 -0500

    Add Half Life as a package

---
+++ half-life/uninstall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2020-present Fewtarius
+
+INSTALLPATH="/storage/roms/ports"
+PORTNAME="half-life"
+
+rm -rf "${INSTALLPATH}/${PORTNAME}"
+rm -f "${INSTALLPATH}/Half Life.sh"
